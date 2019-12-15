44111a880769348083daa5ada8556e693b793f23
Yong Wen Li <ykk3555@gmail.com>
2019-12-15 18:22:32 +0800

    Update home page

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -12,12 +12,13 @@
       <b-navbar-nav class="ml-auto">
 
         <b-navbar-nav>
-          <b-nav-item href="#">Find a tour guide</b-nav-item>
-          <b-nav-item href="#">Find a trip</b-nav-item>
+          <b-nav-item href="#">Tour guides</b-nav-item>
+          <b-nav-item href="#">Trips</b-nav-item>
         </b-navbar-nav>
 
-        <b-nav-item-dropdown text="YWL" right>
+        <b-nav-item-dropdown text="TP012345" right>
           <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">My trips</b-dropdown-item>
           <b-dropdown-item href="#">Sign Out</b-dropdown-item>
         </b-nav-item-dropdown>
       </b-navbar-nav>
--- pages/test.vue
+++ pages/test.vue
@@ -1,11 +1,37 @@
 <template>
-<section class="container">
+  <div>
+  <div id="home-img" class="view">
     <div>
-      <h1>
-        This is the homage page of fyp projet web
-      </h1>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-5 text-left">
+            <div class="card card-search">
+              <h3>Book unique places to stay and things to do.</h3>
+              <form>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">Where</label>
+                  <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Anywhere in Malaysia">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Start date</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="DD/MM/YYYY">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputEmail1">End date</label>
+                  <input type="email" class="form-control" id="exampleInputEmail2" aria-describedby="emailHelp" placeholder="DD/MM/YYYY">
+                </div>
+
+                <button type="submit" class="btn btn-dark float-right">Search</button>
+              </form>
+            </div>
+          </div>
+          <div class="col-lg-6">
+          </div>
+        </div>
+      </div>
     </div>
-  </section>
+  </div>
+  </div>
 </template>
 
 <script>
@@ -18,3 +44,18 @@ export default {
 }
 </script>
 
+<style scoped>
+  #home-img {
+    height: 100%;
+    background: url("../assets/img/kualalumpurcityscape.jpg")no-repeat center center fixed;
+    background-size: cover;
+    min-height: 700px;
+  }
+  .card-search {
+    border: 0;
+    border-radius: 0.3rem;
+    box-shadow: 0 0.5rem 1rem 0 rgba(0, 0, 0, 0.1);
+    margin-top: 100px;
+    padding: 30px;
+  }
+</style>
