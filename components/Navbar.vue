b47c12a23fc2d8dd87b81ab223d390a5c0c5b935
Yong Wen Li <ykk3555@gmail.com>
2019-12-15 16:08:57 +0800

    Create navbar

---
+++ components/Navbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+  <b-navbar toggleable="lg" type="light" variant="light">
+    <b-navbar-brand href="#">APU Tour Guide Booking System</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto">
+
+        <b-navbar-nav>
+          <b-nav-item href="#">Find a tour guide</b-nav-item>
+          <b-nav-item href="#">Find a trip</b-nav-item>
+        </b-navbar-nav>
+
+        <b-nav-item-dropdown text="YWL" right>
+          <b-dropdown-item href="#">Profile</b-dropdown-item>
+          <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+        </b-nav-item-dropdown>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</div>
+</template>
+
+<style scoped>
+
+</style>
+
